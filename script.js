9648042718793aae1a74ea8905a65d85fb63e205
lwinkokointern1 <dkmadsintern1@gmail.com>
2023-03-23 10:12:51 +0630

    update_section

---
+++ script.js
@@ -0,0 +1,25 @@
+function handlerOrientation(e){
+    console.log(e);
+
+    let alpha = e.alpha;
+    let beta = e.beta;
+    let gamma = e.gamma;
+
+    let cube = document.querySelector('.cube');
+    cube.style.transform = 'rotateX(' + beta + 'deg ) rotateY(' + gamma + 'deg ) rotateZ(' + alpha + 'deg )';
+}
+
+async function requestDeviceOrientation(){
+    if(typeof DeviceOrientationEvent != 'undefined' && typeof DeviceOrientationEvent.requestPermission === 'function'){
+        try {
+            const permissionState = await DeviceOrientationEvent.requestPermission();
+            if(permissionState === 'granted'){
+                window.addEventListener('deviceorientation',handlerOrientation);
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }else{
+        window.addEventListener('deviceorientation',handlerOrientation);
+    }
+}
